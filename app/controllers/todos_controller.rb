781a79e9dac3a9247c9bed75b20a0b58d7913cd6
Joseph Tandingan <josephdevapps@gmail.com>
2018-07-30 01:11:57 +0800

    Adding of New Todo functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,30 @@
 class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to @todo
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end
 end
